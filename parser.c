b757bc82e4feb6fab5ae982ca41a6ed835544015
Raslan Darawsheh <rasland@mellanox.com>
2016-09-28 16:48:55 +0300

    updated integer parser
    Signed-off-by: Raslan Darawsheh <rasland@mellanox.com>

--- parser.c
+++ parser.c
@@ -1,20 +1,20 @@
 #include "parser.h"
 #include <stdio.h>
-#include <math.h>
+#include <string.h>
 #include <stdlib.h>
 
 int
 main(int argc, char ** argv){
 	if (argc != 3)
 		return 1;
-	char buf[3][argv[1]];
+	char buf[3][atoi(argv[1])];
 	struct parse_output p ={
-		.size =4,
+		.size = 4,
 		.desc = buf[0],
 		.mask = buf[1],
 		.limit = buf[2],
 	};
-	printf("len = %d", parse_int(argv[2], &p))
+	printf("len = %d\n, p->desc = %d\n", parse_int(argv[2], &p), p.desc);
 	return 0;
 }
 
@@ -28,10 +28,12 @@ char_to_value(char c){
 		return c - 'A' + 10;
 	return 0xff; 
 }
-int parse_int(const char *str, struct parse_output *out){
+int
+parse_int(const char *str, struct parse_output *out){
 	if (!str)
 		return -1;
-	int i, len = 0;
+	int idx = 0, len = 0;
+	char c;
 	int hex = 0;
 	for (len =0; str[len]; len++){
 		c = str[len];
@@ -41,18 +43,42 @@ int parse_int(const char *str, struct parse_output *out){
 		if (c == 'x'){
 			if (!hex){
 				hex = 1;
+				if (len != 1 &&
+					str[len -1] != '0')
+					return -1;
 				continue;
 			}
-			if (len != 1)
-				return -1;
 		}
-
+		if (c == '/' || c == '-'){
+			printf("ranges\n");
+			idx = len;
+			if (hex){
+			len += 2;
+			if (!str[len - 1] || !str[len] ||
+				(str[len - 1] != '0' && str[len] != 'x'))
+				return -1;
+			}
+			continue;
+		}
 		if (char_to_value(c) == 0xff)
 			return -1;
 	}
 	if (!out)
 		return len;
-	printf("%s\n", str);
+	printf("%s\n", str);	
+	int base = ( hex ? 16 : 10);
+	int value1;
+	int value = strtol(str, NULL, base);
+	if(idx)
+		value1 = strtol(&str[idx + 1 ], NULL, base);
+		printf("value1 = %d\n", value1);
+	printf("value = %d\nout->size =%d\n", value, out->size);
+	if ((out->size != 1 && out->size != 2 &&
+		out->size != 4 && out->size != 8) ||
+		(hex && len <= 2))
+		return -1;
+	if(out->desc)
+		memcpy(out->desc, &value, out->size); 
 	return len;
 }
 int
@@ -67,4 +93,6 @@ int
 parse_mac(const char *str, struct parse_output *out){
 	return 0;
 }
+
+
 
