25aa6a9ce4e9f41845d1a7e8ff524687c9d7bd92
Raslan Darawsheh <rasland@mellanox.com>
2016-09-29 10:30:03 +0300

    fixed parser
    Signed-off-by: Raslan Darawsheh <rasland@mellanox.com>

--- parser.c
+++ parser.c
@@ -2,19 +2,22 @@
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
+#include <stdint.h>
 
 int
 main(int argc, char ** argv){
 	if (argc != 3)
 		return 1;
 	char buf[3][atoi(argv[1])];
+
 	struct parse_output p ={
 		.size = 4,
 		.desc = buf[0],
 		.mask = buf[1],
 		.limit = buf[2],
 	};
-	printf("len = %d\n, p->desc = %d\n", parse_int(argv[2], &p), p.desc);
+	printf("len = %d\n", parse_int(argv[2], &p));
+	printf("p->desc = %d\n", *(uint32_t*)p.desc);
 	return 0;
 }
 
@@ -33,31 +36,26 @@ parse_int (const char *str, struct parse_output *out) {
 	if (!str)
 		return -1;
 	int idx = 0, len = 0;
-	char c;
+	char c, rng;
 	int hex = 0;
 	for (len =0; str[len]; len++){
 		c = str[len];
 		if (isspace(c) || c == '#')
 			break;
 
-		if (c == 'x'){
-			if (!hex){
+		if (c == 'x') {			
+			if (!hex || idx) {
 				hex = 1;
-				if (len != 1 &&
-					str[len - 1] != '0')
+				if (len < 1 ||
+					str[len - 1] != '0' ||
+					!str[len + 1])
 					return -1;
 				continue;
 			}
 		}
 		if (c == '/' || c == '-') {
-			printf("ranges\n");
+			rng = c;
 			idx = len;
-			if (hex) {
-			len += 2;
-			if (!str[len - 1] || !str[len] ||
-				(str[len - 1] != '0' && str[len] != 'x'))
-				return -1;
-			}
 			continue;
 		}
 		if (char_to_value(c) == 0xff)
@@ -65,20 +63,19 @@ parse_int (const char *str, struct parse_output *out) {
 	}
 	if (!out)
 		return len;
-	printf("%s\n", str);	
-	int base = (hex ? 16 : 10);
-	int value1;
-	int value = strtol(str, NULL, base);
+	printf("%s\n", str);
+	int limit = -1;
+	int desc = strtol(str, NULL, 0);
 	if (idx)
-		value1 = strtol(&str[idx + 1], NULL, base);
-		printf("value1 = %d\n", value1);
-	printf("value = %d\nout->size =%d\n", value, out->size);
+		limit = strtol(&str[idx + 1], NULL, 0);
+		printf("limit = %d\n", limit);
+	printf("value = %d\nout->size =%d\n", desc, out->size);
 	if ((out->size != 1 && out->size != 2 &&
 		out->size != 4 && out->size != 8) ||
 		(hex && len <= 2))
 		return -1;
 	if (out->desc)
-		memcpy(out->desc, &value, out->size);
+		memcpy(out->desc, &desc, out->size); 
 	return len;
 }
 int
